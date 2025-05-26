d510986055188d9c81f64f7bc7007587fd03fb07
Deymos01 <essenion@gmail.com>
2025-05-26 19:22:41 +0300

    chore: refactor client code

--- cmd/api-client/main.go
+++ cmd/api-client/main.go
@@ -2,14 +2,60 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"flag"
 	"github.com/Deymos01/go-ogen-crud-example/internal/oas"
+	"github.com/go-faster/errors"
 	"log"
 	"net/http"
 )
 
+func createCar(ctx context.Context, client *oas.Client, newCar *oas.NewCar) error {
+	res, err := client.AddCar(ctx, newCar)
+	if err != nil {
+		return errors.Wrap(err, "create car")
+	}
+	log.Printf("Created car: %+v\n", res)
+	return nil
+}
+
+func fetchCar(ctx context.Context, client *oas.Client, id int) error {
+	res, err := client.GetCarById(ctx, oas.GetCarByIdParams{ID: id})
+	if err != nil {
+		return errors.Wrap(err, "get car")
+	}
+	log.Printf("Got car: %+v\n", res)
+	return nil
+}
+
+func updateCar(ctx context.Context, client *oas.Client, id int, car *oas.UpdateCar) error {
+	res, err := client.UpdateCarById(ctx, car, oas.UpdateCarByIdParams{ID: id})
+	if err != nil {
+		return errors.Wrap(err, "update car")
+	}
+	log.Printf("Updated car: %+v\n", res)
+	return nil
+}
+
+func deleteCar(ctx context.Context, client *oas.Client, id int) error {
+	res, err := client.DeleteCarById(ctx, oas.DeleteCarByIdParams{ID: id})
+	if err != nil {
+		return errors.Wrap(err, "delete car")
+	}
+	log.Printf("Deleted car: %+v\n", res)
+	return nil
+}
+
 func main() {
-	client, err := oas.NewClient("http://localhost:8080", oas.WithClient(http.DefaultClient))
+	var err error
+	var arg struct {
+		BaseURL string
+		ID      int
+	}
+	flag.StringVar(&arg.BaseURL, "url", "http://localhost:8080", "target server url")
+	flag.IntVar(&arg.ID, "id", 0, "car id to request")
+	flag.Parse()
+
+	client, err := oas.NewClient(arg.BaseURL, oas.WithClient(http.DefaultClient))
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
@@ -17,40 +63,28 @@ func main() {
 	ctx := context.Background()
 
 	// Add car
-	newCar := oas.NewCar{
-		Manufacturer: "Tesla",
-		Model:        "Model S",
-		Year:         2024,
-		Color:        "White",
-	}
-	createdCar, err := client.AddCar(ctx, &newCar)
+	err = createCar(ctx, client, &oas.NewCar{Manufacturer: "Tesla", Model: "Model S", Year: 2024, Color: "White"})
 	if err != nil {
-		log.Fatalf("failed to add car: %v", err)
+		log.Fatal(err)
 	}
-	fmt.Printf("Created car: %+v\n", createdCar)
 
 	// Get car by ID
-	car, err := client.GetCarById(ctx, oas.GetCarByIdParams{ID: 0})
+	err = fetchCar(ctx, client, arg.ID)
 	if err != nil {
-		log.Fatalf("failed to get car: %v", err)
+		log.Fatal(err)
 	}
-	fmt.Printf("Fetched car: %+v\n", car)
 
 	// Update car by ID
-	updCar := oas.UpdateCar{
+	err = updateCar(ctx, client, arg.ID, &oas.UpdateCar{
 		Model: oas.OptString{Set: true, Value: "Model X"},
-		Color: oas.OptString{Set: true, Value: "Black"},
-	}
-	updRes, err := client.UpdateCarById(ctx, &updCar, oas.UpdateCarByIdParams{ID: 0})
+		Color: oas.OptString{Set: true, Value: "Black"}})
 	if err != nil {
-		log.Fatalf("failed to update car: %v", err)
+		log.Fatal(err)
 	}
-	fmt.Printf("Updated car: %+v\n", updRes)
 
 	// Delete car by ID
-	delRes, err := client.DeleteCarById(ctx, oas.DeleteCarByIdParams{ID: 0})
+	err = deleteCar(ctx, client, arg.ID)
 	if err != nil {
-		log.Fatalf("failed to delete car: %v", err)
+		log.Fatal(err)
 	}
-	fmt.Printf("Delete result: %T\n", delRes)
 }
